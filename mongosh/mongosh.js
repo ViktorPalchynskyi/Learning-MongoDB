de6f5d5d550dd0ea70ef5e0acce604fbb53325ea
Viktor <palchinskij19@gmail.com>
2024-09-05 15:20:46 +0300

    05.09.24 - finding places inside a certain area

--- mongosh/mongosh.js
+++ mongosh/mongosh.js
@@ -523,3 +523,14 @@ db.places.find({
         },
     },
 });
+
+db.places.find({
+    location: {
+        $geoWithin: {
+            $geometry: {
+                type: 'Polygon',
+                coordinates: [[p1, p2, p3, p4, p1]],
+            },
+        },
+    },
+});
