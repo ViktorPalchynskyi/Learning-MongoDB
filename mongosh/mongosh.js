f7b01a94e533ce111b634421fe0c92f12e5d8cc6
Viktor <palchinskij19@gmail.com>
2024-09-06 14:54:23 +0300

    06.09.24 - applying multiple operatins to an array

--- mongosh/mongosh.js
+++ mongosh/mongosh.js
@@ -804,3 +804,24 @@ db.friends.aggregate([
         },
     },
 ]);
+
+db.friends.aggregate([
+    { $unwind: '$examScores' },
+    {
+        $project: {
+            _id: 1,
+            name: 1,
+            age: 1,
+            score: '$examScores.score',
+        },
+    },
+    { $sort: { score: -1 } },
+    {
+        $group: {
+            _id: '$_id',
+            name: { $first: '$name' },
+            maxScore: { $max: '$score' },
+        },
+    },
+    { $sort: { maxScore: -1 } },
+]);
